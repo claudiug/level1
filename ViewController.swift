66d4d8e54b082841f176efc9527ddb14b2e4eecb
claudiu <claudiu.garba@gmail.com>
2015-12-27 23:35:53 +0100

    not working

--- ViewController.swift
+++ ViewController.swift
@@ -2,53 +2,16 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
+    let mapView: MKMapView = MKMapView()
+    
 
   override func viewDidLoad() {
     super.viewDidLoad()
     self.view.backgroundColor = UIColor.lightGrayColor()
     print(self.view.bounds)
-//    let view1 = UIView(frame: CGRect(origin: CGPoint(x: 100, y: 200), size: CGSize(width: 200, height: 200)))
-//    let view2 = UIView(frame: CGRect(origin: CGPoint(x: 100, y: 100), size: CGSize(width: 50, height: 50)))
-//    view1.backgroundColor = UIColor.redColor()
-//    view2.backgroundColor = UIColor.orangeColor()
-//    view1.alpha = CGFloat(22)
-//    view1.tintColor = UIColor.blueColor()
-//    view1.addSubview(view2)
-//    self.view.addSubview(view1)
-//    print(self.view.subviews)
-//    print(self.view.superview)
-//    print(view2.superview)
-
-//    let label = UILabel()
-//    label.text = "this is a label"
-//    label.font = UIFont(name: "Menlo", size: 23)
-//    label.backgroundColor = UIColor.blackColor()
-//    label.textColor = UIColor.greenColor()
-//    label.textAlignment = .Center
-//    label.frame = CGRectMake(12, 12, 200, 100)
-    //button
-
-//    let button = UIButton()
-//    button.setTitle("Amazing", forState: .Normal)
-//    button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-//    button.frame = CGRect(origin: CGPoint(x: 100, y: 200), size: CGSize(width: 200, height: 200))
-//
-//    button.addTarget(self, action: "pressed:", forControlEvents: .TouchUpInside)
-//
-//    self.view.addSubview(button)
-//
-//  }
-//
-//  func pressed(sender: UIButton) {
-//    var alertView = UIAlertView()
-//    alertView.addButtonWithTitle("OK")
-//    alertView.title = "title"
-//    alertView.message = "message"
-//    alertView.show()
-
-    self.view = MKMapView()
-
+    self.view = mapView
     let button = UIButton()
+    
     button.setTitle("Amazing", forState: .Normal)
     button.setTitleColor(UIColor.blueColor(), forState: .Normal)
     button.frame = CGRect(origin: CGPoint(x: 100, y: 200), size: CGSize(width: 200, height: 200))
@@ -56,14 +19,24 @@ class ViewController: UIViewController {
     
     self.view.addSubview(button)
 
-    let topConstraint = button.topAnchor.constraintEqualToAnchor(self.view.topAnchor)
-    let leadingConstraint = button.leadingAnchor.constraintEqualToAnchor(self.view.leadingAnchor)
-    let trallingConstraint = button.trailingAnchor.constraintEqualToAnchor(self.view.trailingAnchor)
+//    let topConstraint = button.topAnchor.constraintEqualToAnchor(self.view.topAnchor)
+    let topConstraint = button.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)
+//    let leadingConstraint = button.leadingAnchor.constraintEqualToAnchor(self.view.leadingAnchor)
+//    let trallingConstraint = button.trailingAnchor.constraintEqualToAnchor(self.view.trailingAnchor)
+    
+    let margins = self.view.layoutMarginsGuide
+    let leadingConstraint = button.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+    let trallingConstraint = button.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
     
     topConstraint.active = true
     leadingConstraint.active = true
     trallingConstraint.active = true
     
+    button.addTarget(self, action: "changeMap", forControlEvents: .TouchUpInside)
   }
+    
+    func changeMap(sender: UIButton) {
+       mapView.mapType = .Hybrid
+    }
 
 }
